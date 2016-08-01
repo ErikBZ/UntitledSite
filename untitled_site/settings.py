56075f13ec4b59374cb5b9d34eb73bc93ee33b2a
Erik Zapata <zapatabrandon@gmail.com>
2016-08-01 18:17:17 -0400

    Managed to finally set up mysql. Creating the replies models now and displaying those

--- untitled_site/settings.py
+++ untitled_site/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'untitled_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'untitled_site',
+        'USER': 'thesuperfox',
+        'PASSWORD': 'Somethingaboutus1.',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
